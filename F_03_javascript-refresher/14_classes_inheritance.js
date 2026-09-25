95e94afb2e90799baa4f9a19b8d5d0284066f3ac
Daniel Casimiro <danierukamishiro@gmail.com>
2026-09-25 14:10:44 +0800

    Done with 14_classes_inheritance.js part

---
+++ F_03_javascript-refresher/14_classes_inheritance.js
@@ -0,0 +1,18 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  sayHello() {
+    console.log("Hi, I am " + this.name);
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(this.name + " is studying JavaScript.");
+  }
+}
+
+const learner = new Student("Miguel");
+learner.sayHello();
+learner.study();
