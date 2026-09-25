d078a7218d181f9cda806b60f9f2a7079ea49433
Daniel Casimiro <danierukamishiro@gmail.com>
2026-09-25 14:09:12 +0800

    Done with 11_arrow_functions.js part

---
+++ F_03_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,10 @@
+const greet = name => "Hey there, " + name + "!";
+const square = n => n * n;
+
+const sayHi = () => {
+  console.log("Kumusta!");
+};
+
+console.log(greet("Dan"));
+console.log(square(7));
+sayHi();
