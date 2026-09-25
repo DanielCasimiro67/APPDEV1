a155cacd705d603a6eea32f0f2519455285c03a7
Daniel Casimiro <danierukamishiro@gmail.com>
2026-09-25 14:08:44 +0800

    Done with 10_let_const.js part

---
+++ F_03_javascript-refresher/10_let_const.js
@@ -0,0 +1,11 @@
+let name = "Daniel";
+const age = 21;
+
+name = "Dan"; // OK
+console.log(name);
+
+// age = 22; // Error: Assignment to constant variable
+console.log(age);
+
+var city = "Angeles City"; // works, but avoid var
+console.log(city);
