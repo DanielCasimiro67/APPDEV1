a3d0383953d0331c1047b75b35932041561ed088
Daniel Casimiro <danierukamishiro@gmail.com>
2026-09-25 13:58:57 +0800

    Done with 02_variables.js part

---
+++ F_03_javascript-refresher/02_variables.js
@@ -0,0 +1,14 @@
+let subject = "JavaScript";
+let year = 2;
+let isEnrolled = true;
+
+console.log(subject, typeof subject);
+console.log(year, typeof year);
+console.log(isEnrolled, typeof isEnrolled);
+
+let x = 12, y = 4;
+console.log("Add:", x + y);
+console.log("Divide:", x / y);
+
+console.log("4" == 4);  // true
+console.log("4" === 4); // false
